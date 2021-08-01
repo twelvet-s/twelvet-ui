f80aef1aa0a9472e5737e59184a13c46908d9e7f
twelvet <2471835953@qq.com>
2021-08-01 12:00:34 +0800

    加载效果

--- src/components/RightContent/index.tsx
+++ src/components/RightContent/index.tsx
@@ -28,18 +28,9 @@ const GlobalHeaderRight: React.FC = () => {
                 placeholder="站内搜索"
                 defaultValue="umi ui"
                 options={[
-                    { label: <a href="https://umijs.org/zh/guide/umi-ui.html">umi ui</a>, value: 'umi ui' },
                     {
-                        label: <a href="next.ant.design">Ant Design</a>,
-                        value: 'Ant Design',
-                    },
-                    {
-                        label: <a href="https://protable.ant.design/">Pro Table</a>,
-                        value: 'Pro Table',
-                    },
-                    {
-                        label: <a href="https://prolayout.ant.design/">Pro Layout</a>,
-                        value: 'Pro Layout',
+                        label: <a href="//www.twelvet.cn" target='_blank'>TwelveT</a>,
+                        value: 'TwelveT',
                     },
                 ]}
             // onSearch={value => {
@@ -47,9 +38,10 @@ const GlobalHeaderRight: React.FC = () => {
             // }}
             />
             <span
+                title='Swagger聚合文档'
                 className={styles.action}
                 onClick={() => {
-                    window.open('https://pro.ant.design/docs/getting-started');
+                    window.open('/api/doc.html');
                 }}
             >
                 <QuestionCircleOutlined />
