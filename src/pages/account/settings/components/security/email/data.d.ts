775ffd1a9d91d3700adb0b5d3647421a4331a5a4
twelvet <2471835953@qq.com>
2022-08-28 19:25:35 +0800

    v6

--- src/pages/account/settings/components/security/email/data.d.ts
+++
@@ -1,6 +0,0 @@
-export interface EmailType {
-    // 是否显示窗口
-    emailModal: boolean
-    // 点击取消按钮时触发
-    onCancel: () => void
-}
--- src/pages/account/settings/components/security/password/data.d.ts
+++
@@ -1,6 +0,0 @@
-export interface PasswordType {
-    // 是否显示窗口
-    passwordModal: boolean
-    // 点击取消按钮时触发
-    onCancel: () => void
-}
