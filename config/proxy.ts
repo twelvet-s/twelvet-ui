d02c07194fcc2a2467c92eb6846354d09f433379
twelvet <2471835953@qq.com>
2022-09-03 17:12:02 +0800

    fix: 切换到线上平台

--- config/proxy.ts
+++ config/proxy.ts
@@ -14,7 +14,7 @@ export default {
     // localhost:8000/api/** -> https://preview.pro.ant.design/api/**
     '/api/': {
       // 要代理的地址
-      target: 'http://localhost:8080',
+      target: 'http://cloud.twelvet.cn/api',
       // 配置了这个可以从 http 代理到 https
       // 依赖 origin 的功能可能需要这个，比如 cookie
       changeOrigin: true,
