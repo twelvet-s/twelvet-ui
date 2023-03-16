70380adea352f5b2ac853d3bf7cbfd34e9ebe724
TwelveT <2471835953@qq.com>
2023-03-16 09:16:30 +0800

    优化TS

--- src/pages/Tool/Gen/components/PreviewCode/Index.tsx
+++ src/pages/Tool/Gen/components/PreviewCode/Index.tsx
@@ -1,7 +1,7 @@
-import React, {useState, useEffect} from 'react'
-import {Modal, Tabs, Skeleton} from 'antd'
-import {getInfo} from './service'
-import {system} from '@/utils/twelvet'
+import React, { useState, useEffect } from 'react'
+import { Modal, Tabs, Skeleton } from 'antd'
+import { getInfo } from './service'
+import { system } from '@/utils/twelvet'
 import styles from './styles.less'
 
 /**
@@ -15,9 +15,31 @@ const PreviewCode: React.FC<{
   onClose: () => void
 }> = (props) => {
 
-  const {info, onClose} = props
+  const { info, onClose } = props
 
-  const [codeData, setCodeData] = useState<{}>({})
+  const [codeData, setCodeData] = useState<{
+    'vm/java/controller.java.vm': string
+    'vm/java/service.java.vm': string
+    'vm/java/serviceImpl.java.vm': string
+    'vm/java/mapper.java.vm': string
+    'vm/xml/mapper.xml.vm': string
+    'vm/java/domain.java.vm': string
+    'vm/react/index.tsx.vm': string
+    'vm/react/index-tree.tsx.vm': string
+    'vm/js/api.ts.vm': string
+    'vm/sql/sql.vm': string
+  }>({
+    'vm/java/controller.java.vm': '',
+    'vm/java/service.java.vm': '',
+    'vm/java/serviceImpl.java.vm': '',
+    'vm/java/mapper.java.vm': '',
+    'vm/xml/mapper.xml.vm': '',
+    'vm/java/domain.java.vm': '',
+    'vm/react/index.tsx.vm': '',
+    'vm/react/index-tree.tsx.vm': '',
+    'vm/js/api.ts.vm': '',
+    'vm/sql/sql.vm': '',
+  })
 
   const [loading, setLoading] = useState<boolean>(false)
 
@@ -28,7 +50,7 @@ const PreviewCode: React.FC<{
   const refGetInfo = async (tableId: number) => {
     try {
       setLoading(true)
-      const {data} = await getInfo(tableId)
+      const { data } = await getInfo(tableId)
 
       setCodeData(data)
     } catch (e) {
@@ -63,91 +85,91 @@ const PreviewCode: React.FC<{
           tabPosition='top'
         >
           <Tabs.TabPane tab="Controller.java" key="1">
-              <pre className={styles.preCode}>
-                  <code>
-                    {codeData['vm/java/controller.java.vm']}
-                  </code>
-              </pre>
+            <pre className={styles.preCode}>
+              <code>
+                {codeData['vm/java/controller.java.vm']}
+              </code>
+            </pre>
           </Tabs.TabPane>
 
           <Tabs.TabPane tab="Service.java" key="2">
-              <pre className={styles.preCode}>
-                  <code>
+            <pre className={styles.preCode}>
+              <code>
 
-                    {codeData['vm/java/service.java.vm']}
+                {codeData['vm/java/service.java.vm']}
 
-                  </code>
-              </pre>
+              </code>
+            </pre>
           </Tabs.TabPane>
 
           <Tabs.TabPane tab="ServiceImpl.java" key="3">
-              <pre className={styles.preCode}>
-                  <code>
+            <pre className={styles.preCode}>
+              <code>
 
-                    {codeData['vm/java/serviceImpl.java.vm']}
+                {codeData['vm/java/serviceImpl.java.vm']}
 
-                  </code>
-              </pre>
+              </code>
+            </pre>
           </Tabs.TabPane>
 
           <Tabs.TabPane tab="Mapper.java" key="4">
-              <pre className={styles.preCode}>
-                  <code>
+            <pre className={styles.preCode}>
+              <code>
 
-                    {codeData['vm/java/mapper.java.vm']}
+                {codeData['vm/java/mapper.java.vm']}
 
-                  </code>
-              </pre>
+              </code>
+            </pre>
           </Tabs.TabPane>
 
           <Tabs.TabPane tab="Mapper.xml" key="5">
-              <pre className={styles.preCode}>
-                  <code>
+            <pre className={styles.preCode}>
+              <code>
 
-                    {codeData['vm/xml/mapper.xml.vm']}
+                {codeData['vm/xml/mapper.xml.vm']}
 
-                  </code>
-              </pre>
+              </code>
+            </pre>
           </Tabs.TabPane>
 
           <Tabs.TabPane tab="Domain.java" key="6">
-              <pre className={styles.preCode}>
-                  <code>
+            <pre className={styles.preCode}>
+              <code>
 
-                    {codeData['vm/java/domain.java.vm']}
+                {codeData['vm/java/domain.java.vm']}
 
-                  </code>
-              </pre>
+              </code>
+            </pre>
           </Tabs.TabPane>
 
           <Tabs.TabPane tab={codeData['vm/react/index.tsx.vm'] ? 'index.tsx' : 'index-tree.tsx'} key="7">
-              <pre className={styles.preCode}>
-                  <code>
+            <pre className={styles.preCode}>
+              <code>
 
-                    {codeData['vm/react/index.tsx.vm'] ? codeData['vm/react/index.tsx.vm'] : codeData['vm/react/index-tree.tsx.vm']}
+                {codeData['vm/react/index.tsx.vm'] ? codeData['vm/react/index.tsx.vm'] : codeData['vm/react/index-tree.tsx.vm']}
 
-                  </code>
-              </pre>
+              </code>
+            </pre>
           </Tabs.TabPane>
 
           <Tabs.TabPane tab="Api.ts" key="8">
-              <pre className={styles.preCode}>
-                  <code>
+            <pre className={styles.preCode}>
+              <code>
 
-                    {codeData['vm/js/api.ts.vm']}
+                {codeData['vm/js/api.ts.vm']}
 
-                  </code>
-              </pre>
+              </code>
+            </pre>
           </Tabs.TabPane>
 
           <Tabs.TabPane tab="SQL" key="9">
-              <pre className={styles.preCode}>
-                  <code>
+            <pre className={styles.preCode}>
+              <code>
 
-                    {codeData['vm/sql/sql.vm']}
+                {codeData['vm/sql/sql.vm']}
 
-                  </code>
-              </pre>
+              </code>
+            </pre>
           </Tabs.TabPane>
 
         </Tabs>
