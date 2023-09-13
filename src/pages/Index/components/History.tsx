a308aae5395556446b3fd00efbf91a3d3ae15d73
TwelveT <2471835953@qq.com>
2023-09-13 10:14:37 +0800

    优化历史记录显示

--- src/pages/Index/components/History.tsx
+++ src/pages/Index/components/History.tsx
@@ -1,13 +1,16 @@
-import { Collapse } from 'antd';
+import { Collapse, CollapseProps } from 'antd';
 import styles from './styles.less';
 
 /**
  * TwelveT发展史组件
  */
 const History = () => {
-    return (
-        <Collapse defaultActiveKey={11} className={styles.historyOl}>
-            <Collapse.Panel header="v3.0.5 - 2023-7-31" key={11}>
+
+    const items: CollapseProps['items'] = [
+        {
+            key: 11,
+            label: 'v3.0.5 - 2023-7-31',
+            children: (
                 <ol>
                     <li>即将全面切换JDK 17，预备JDK 21虚拟线程</li>
                     <li>Spring Cloud Alibaba 兼容 Spring Boot3 Releases</li>
@@ -16,8 +19,12 @@ const History = () => {
                     <li>修复代码生成器，兼容Antd Pro</li>
                     <li>修复已知BUG</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v3.0.0 - 2023-1-22" key={10}>
+            ),
+        },
+        {
+            key: 10,
+            label: 'v3.0.0 - 2023-1-22',
+            children: (
                 <ol>
                     <li>升级Spring Boot3</li>
                     <li>升级JDK最低要求17</li>
@@ -33,8 +40,12 @@ const History = () => {
                     <li>升级pom依赖</li>
                     <li>其他细节优化</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v2.6.0 - 2022-7-28" key={9}>
+            ),
+        },
+        {
+            key: 9,
+            label: 'v2.6.0 - 2022-7-28',
+            children: (
                 <ol>
                     <li>替换OAuth2最新依赖OAuth2 Authorization Server</li>
                     <li>升级Srping Boot2.7，做好jdk17大版本升级</li>
@@ -46,8 +57,12 @@ const History = () => {
                     <li>网关增加日志Debug</li>
                     <li>其他细节优化</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v2.5.0 - 2022-3-31" key={8}>
+            ),
+        },
+        {
+            key: 8,
+            label: 'v2.5.0 - 2022-3-31',
+            children: (
                 <ol>
                     <li>优化docker-compose一件启动体验</li>
                     <li>新增docker一件启动脚本</li>
@@ -61,8 +76,12 @@ const History = () => {
                     <li>新增分布式锁注解</li>
                     <li>其他细节优化</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v2.0.0 - 2021-10-01" key={7}>
+            ),
+        },
+        {
+            key: 7,
+            label: 'v2.0.0 - 2021-10-01',
+            children: (
                 <ol>
                     <li>jenkins一件打包发布</li>
                     <li>docker-compose启动</li>
@@ -73,8 +92,12 @@ const History = () => {
                     <li>修复部门选择BUG</li>
                     <li>修复已知BUG</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v1.3.0 - 2021-08-13" key={6}>
+            ),
+        },
+        {
+            key: 6,
+            label: 'v1.3.0 - 2021-08-13',
+            children: (
                 <ol>
                     <li>前端UI升级至antdV5</li>
                     <li>更改request使用</li>
@@ -84,8 +107,12 @@ const History = () => {
                     <li>新增redis监控面板</li>
                     <li>修复已知BUG</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v1.2.0 - 2021-06-23" key={5}>
+            ),
+        },
+        {
+            key: 5,
+            label: 'v1.2.0 - 2021-06-23',
+            children: (
                 <ol>
                     <li>新增菜单导航栏</li>
                     <li>修复阿里ICON无法显示BUG</li>
@@ -95,8 +122,12 @@ const History = () => {
                     <li>加入使用文档</li>
                     <li>修复已知BUG</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v1.1.0 - 2021-04-07" key={4}>
+            ),
+        },
+        {
+            key: 4,
+            label: 'v1.1.0 - 2021-04-07',
+            children: (
                 <ol>
                     <li>新增代码生成器</li>
                     <li>修复多文件上传无文件时出现的错误响应</li>
@@ -108,8 +139,12 @@ const History = () => {
                     <li>修改nacos配置参数</li>
                     <li>增加分布式事务</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v1.0.2 - 2021-03-06" key={3}>
+            ),
+        },
+        {
+            key: 3,
+            label: 'v1.0.2 - 2021-03-06',
+            children: (
                 <ol>
                     <li>加入TwelveT icon</li>
                     <li>修复全局layouts驼峰导致的打包敏感BUG</li>
@@ -121,18 +156,30 @@ const History = () => {
                     <li>离线icon支持</li>
                     <li>更新SQL</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v1.0.1 - 2021-02-11" key={2}>
+            ),
+        },
+        {
+            key: 2,
+            label: 'v1.0.1 - 2021-02-11',
+            children: (
                 <ol>
                     <li>TwelveT正式对外发布</li>
                 </ol>
-            </Collapse.Panel>
-            <Collapse.Panel header="v1.0.0 - 2019-10-27" key={1}>
+            ),
+        },
+        {
+            key: 1,
+            label: 'v1.0.0 - 2019-10-27',
+            children: (
                 <ol>
                     <li>TwelveT正式建立</li>
                 </ol>
-            </Collapse.Panel>
-        </Collapse>
+            ),
+        },
+    ];
+
+    return (
+        <Collapse items={items} defaultActiveKey={[11]} className={styles.historyOl} />
     );
 };
 
