7494330dc00dfe5fa6cd962c6d803d75361ceaa2
twelvet <2471835953@qq.com>
2024-10-27 20:24:45 +0800

    [add] ai知识库

--- src/pages/AI/Chat/index.tsx
+++ src/pages/AI/Chat/index.tsx
@@ -1,18 +1,43 @@
 import { PageContainer } from '@ant-design/pro-components';
-import React, {useState, useRef} from 'react';
+import { Button } from 'antd';
+import React, { useState, useRef, useEffect } from 'react';
 
 /**
  * AI助手模块
  */
-const Post: React.FC = () => {
+const AIChat: React.FC = () => {
 
-    
+    const [sseData, setSSEData] = useState<[]>([]);
+
+    const sse = () => {
+        const eventSource = new EventSource('http://localhost:8000/api/ai/chat?q=你好');
+
+        eventSource.onmessage = (event) => {
+            // 处理接收到的数据
+            const json = JSON.parse(event.data)
+            setSSEData((prevData) => [...prevData, json]); // 更新状态 
+            console.log(`==${new Date()}==`, sseData)
+        };
+
+        eventSource.onerror = (event) => {
+            console.error("EventSource failed:", event);
+            eventSource.close();
+        };
+    }
 
     return (
         <PageContainer>
-           
+            <Button onClick={sse}>发起sse</Button>
+            <div>
+                <h1>SSE Data</h1>
+                {
+                    sseData.map((item, i) => (
+                        item.result.output.content
+                    ))
+                }
+            </div>
         </PageContainer>
     );
 };
 
-export default Post;
+export default AIChat;
--- src/pages/AI/Chat/service.ts
+++ src/pages/AI/Chat/service.ts
@@ -1,4 +1,5 @@
 import {request} from '@umijs/max'
+import { ErrorHandler, MessageHandler, SSE } from '@/api/sse';
 
 // 请求的控制器名称
 const controller = "/ai/chat";
